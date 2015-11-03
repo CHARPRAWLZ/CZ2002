9703ed45362fef4a22d8fa912cf169cc0380fc28
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-03 15:20:14 +0800

    Revert "deleting files"
    This reverts commit d1d2051020c19cbd155cbbc4856f5b10ada2097c.

---
+++ AppUI.java
@@ -0,0 +1,4 @@
+public class AppUI {
+   public Cineplex getCineplexID;
+   
+}
---
+++ Movie.java
@@ -0,0 +1,5 @@
+public class Movie {
+    private String title, synopsis, director, cast, type;
+    private double overallRating;
+    
+}

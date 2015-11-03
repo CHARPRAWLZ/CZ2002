a84e9a8861372d0c37c74bc81bb164b6f160f4be
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-03 15:10:39 +0800

    Initial Files
    Copied from Dropbox

---
+++ Cinema.java
@@ -0,0 +1,10 @@
+public class Cinema {
+    private String name;
+    
+    public String getName(){
+    return name;
+    }
+    public void setName(String Name){
+    name = Name;
+    }
+}
---
+++ CinemaStaff.java
@@ -0,0 +1,9 @@
+public class CinemaStaff {
+    private String staffID, staffName, password;
+    
+    public void configureTickets(){}
+    public void configureMovies(){}
+    public void configureTop5movies(){}
+    public void configureMovieListing(){}
+    
+}

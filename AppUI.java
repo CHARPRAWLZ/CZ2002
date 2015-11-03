d1d2051020c19cbd155cbbc4856f5b10ada2097c
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-03 15:16:32 +0800

    deleting files
    deleting files simulating on branch

--- AppUI.java
+++
@@ -1,4 +0,0 @@
-public class AppUI {
-   public Cineplex getCineplexID;
-   
-}
--- Movie.java
+++
@@ -1,5 +0,0 @@
-public class Movie {
-    private String title, synopsis, director, cast, type;
-    private double overallRating;
-    
-}

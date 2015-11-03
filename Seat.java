a340c464b33fced93423d51ff6a84d47163ab308
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-03 23:07:22 +0800

    Seat
    Added constructor, mutator and accessors

--- Seat.java
+++ Seat.java
@@ -5,4 +5,21 @@ public class Seat {
     private int seatNumber;
     private boolean isBooked;
     
+    Seat(int seatNumber) {
+        this.seatNumber = seatNumber;
+        isBooked = false;
+    }
+    
+    public int getSeatNumber() {
+        return this.seatNumber;
+    }
+    
+    public boolean seatIsBooked() {
+        return this.isBooked;
+    }
+    
+    public void setBookStatus(boolean booking) {
+        this.isBooked = booking;
+    }
+    
 }

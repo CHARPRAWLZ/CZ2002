68eb19ae6273de51b640599efc0a2a6238209b3a
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-04 16:39:28 +0800

    Revert "Edited Seat class"
    This reverts commit 171d612ea7630cb6a9d794d38b97c2e9e5b5814d.

--- Seat.java
+++ Seat.java
@@ -1,48 +1,25 @@
 package CZ2002;
 
 public class Seat {
-
-    private char seatRow;
+    
     private int seatNumber;
     private boolean isBooked;
-    private final boolean exist;
     
-    Seat(char seatRow, int seatNumber) {
-        this(seatRow, seatNumber, true);
-    }
-    
-    Seat(String seatID) {
-        this(seatID, true);
-    }
-
-    Seat(char seatRow, int seatNumber, boolean exist) {
-        this.seatRow = seatRow;
+    Seat(int seatNumber) {
         this.seatNumber = seatNumber;
-        this.exist = exist;
+        isBooked = false;
     }
-
-    Seat(String seatID, boolean exist) {
-        this(seatID.charAt(0), Integer.valueOf(seatID.substring(1)), exist);
-    }
-
+    
     public int getSeatNumber() {
         return seatNumber;
     }
-
-    public char getSeatRow() {
-        return seatRow;
-    }
-
+    
     public boolean seatIsBooked() {
         return isBooked;
     }
-
+    
     public void setBookStatus(boolean booking) {
         isBooked = booking;
     }
-
-    public boolean seatExist() {
-        return exist;
-    }
-
+    
 }

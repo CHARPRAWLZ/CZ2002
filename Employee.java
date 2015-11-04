f522278ce040c88a07ca1689c72175df62c5d8b5
zacharias60 <zacharias60@gmail.com>
2015-11-04 12:30:05 +0800

    changes to employee

--- Employee.java
+++ Employee.java
@@ -1,9 +1,9 @@
 package CZ2002;
 
 public class Employee extends User{
-    private String userName,password;
-    public Employee(String user,String pw){
-        
+    private String userName,password,name,emailAddress,mobileNo;
+    public Employee(String user,String pw,String name,String email, String no){
+        super(name,emailAddress,mobileNo);
         this.userName = user;
         this.password = pw;
     }

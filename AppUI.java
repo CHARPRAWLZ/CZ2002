a58e2412edc5dc62b2ef4aeb55c86a5c5fca5a1d
fieana <alfie_farhana91@hotmail.com>
2015-11-04 21:21:25 +0800

    revamp

--- AppUI.java
+++ AppUI.java
@@ -1,58 +1,55 @@
 package CZ2002;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class AppUI {
 
+    static Scanner sc = new Scanner(System.in);
     public Cineplex getCineplexID;
 
     public static void main(String args[]) {
-        int choice;
-        String username, password;
-        Scanner sc = new Scanner(System.in);
+        String choice;
 
         // Welcome page to MOBLIMA
         System.out.println("Welcome to MOBLIMA");
-        System.out.println("1. Login");
-        System.out.println("2. Sign Up");
-        System.out.println("3. Quit");
-
-        System.out.print("Enter choice: ");
-        choice = sc.nextInt();
-
         do {
+            System.out.println("1. Login as Admin");
+            System.out.println("2. Browse as Movie goer");
+            System.out.println("3. Quit");
 
+            System.out.print("Enter choice: ");
+            choice = sc.nextLine();
             switch (choice) {
-                case 1:
-                    loginUI();
-                    choice = 3;
+                case "1":
+                    adminUI();
+                    choice = "3";
                     break;
-                case 2:
-                    signUpUI();
-                    choice = 3;
+                case "2":
+                    movieGoerUI();
+                    choice = "3";
                     break;
-                case 3:
+                case "3":
                     break;
                 default:
-                    System.out.println("Error input!");
-                    System.out.println("\n1. Login");
-                    System.out.println("2. Sign Up");
-                    System.out.println("3. Quit");
-                    System.out.print("Enter choice: ");
-                    choice = sc.nextInt();
+                    System.out.println("\nEnter a valid input!");
             }
 
-        } while (choice != 3);
-        
+        } while (!choice.equals("3"));
         System.out.println("\n----------------------------------------");
         System.out.println("Thank you for using MOBLIMA");
-    }
-
-    private static void loginUI() {
 
     }
 
-    private static void signUpUI() {
+    private static void adminUI() {
+        System.out.println("\n----------------------------------------");
+        System.out.println("Login as Admin");
+        String username;
+        System.out.print("Username: ");
+        username = sc.nextLine();
+    }
 
+    private static void movieGoerUI() {
+        System.out.println("movieGoerUI");
     }
 }

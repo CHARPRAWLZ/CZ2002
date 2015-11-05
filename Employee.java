fcffd9223c24a08e66a61857de8fae4b49eb6214
zacharias60 <zacharias60@gmail.com>
2015-11-05 20:00:05 +0800

    CinemaStaff delete, methods copied to Employee
    Deleted cinemastaff, edited the methods in employee, not too sure if the
    methods are correct

--- Employee.java
+++ Employee.java
@@ -1,5 +1,5 @@
 package CZ2002;
-
+import java.util.Scanner;
 public class Employee extends User{
     private String userName, password;
     public Employee(String user, String pw, String id, String name, String email,String no){
@@ -7,6 +7,21 @@ public class Employee extends User{
         this.userName = user;
         this.password = pw;
     }
+    public Movie movie;
+    Scanner sc = new Scanner(System.in);
+    public void configureTickets(){}
+    public void configureMovies(){
+        String status = sc.next();
+        movie.setMovieStatus(status);
+    }
+    public void configureTop5movies(){
+        double rating = sc.nextDouble();
+        movie.setOverallRating(rating);
+    }
+    public void configureMovieListing(){
+       movie.getMovieStatus();
+    }
+    
     
     //
     //testing

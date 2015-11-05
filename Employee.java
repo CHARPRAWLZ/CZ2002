1c3762d49e3a9df6f76ffd4fda8dfe5661290b19
zacharias60 <zacharias60@gmail.com>
2015-11-05 19:33:08 +0800

    Same as master
    deleted customer and edited employee

--- Employee.java
+++ Employee.java
@@ -1,12 +1,13 @@
 package CZ2002;
 
 public class Employee extends User{
-    private String userName,password,name,emailAddress,mobileNo;
-    public Employee(String user,String pw,String name,String email, String no){
-        super(name,emailAddress,mobileNo);
+    private String userName, password;
+    public Employee(String user, String pw, String id, String name, String email,String no){
+        super(id,name,email,no);
         this.userName = user;
         this.password = pw;
     }
+    
     //
     //testing
 }

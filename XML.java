4d53efd7cd1b142dc3e314fcca7003da0873a2de
fieana <alfie_farhana91@hotmail.com>
2015-11-06 01:20:15 +0800

    add comments

--- XML.java
+++ XML.java
@@ -25,8 +25,8 @@ public class XML {
     }
 
     public XML(String f) {
+        //create document
         try {
-            //create document
             DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             this.file = new File(f);
             this.doc = dBuilder.newDocument();
@@ -43,45 +43,46 @@ public class XML {
         Element root = xml.setRoot("allmovies");
         String choice = "-1";
         Scanner sc = new Scanner(System.in);
-        try {
-            // setTitle()
-            do {
-                System.out.println("1: Input Movie");
-                System.out.println("0: Exit");
-                System.out.print("Enter choice: ");
-                choice = sc.nextLine();
-                switch (choice) {
-                    case "1":
-                        Element movie = xml.addNewChild(root, "movie");
-                        xml.setIncId(movie);
+        // setTitle()
+        do {
+            System.out.println("1: Input Movie");
+            System.out.println("0: Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextLine();
+            switch (choice) {
+                case "1":
+                    Element movie = xml.addNewChild(root, "movie");
+                    xml.setIncId(movie);
 
-                        System.out.print("Enter title: ");
-                        String titleIn = sc.nextLine();
-                        xml.addElement(movie, "title", titleIn);
+                    System.out.print("Enter title: ");
+                    String titleIn = sc.nextLine();
+                    xml.addElement(movie, "title", titleIn);
 
-                        System.out.print("Enter status: ");
-                        String statusIn = sc.nextLine();
-                        xml.addElement(movie, "status", statusIn);
+                    System.out.print("Enter status: ");
+                    String statusIn = sc.nextLine();
+                    xml.addElement(movie, "status", statusIn);
 
-                        xml.writeContent();
+                    xml.writeContent();
 
-                        break;
-                    case "0":
-                        break;
-                }
+                    break;
+                case "0":
+                    break;
+            }
 
-            } while (!choice.equals("0"));
+        } while (!choice.equals("0"));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
+    /**
+     * set root, and add counter
+     *
+     * @param r
+     * @return
+     */
     public Element setRoot(String r) {
         Element eRoot = this.doc.createElement(r);
-        if (this.file.exists()) {
-            eRoot = this.doc.getDocumentElement();
-        } else {
+        if (this.file.exists()) eRoot = this.doc.getDocumentElement();
+        else {
             eRoot.setAttribute("counter", "0");
             this.doc.appendChild(eRoot);
         }
@@ -89,16 +90,11 @@ public class XML {
         return this.root;
     }
 
-    public Element addNewChild(Element parent, String child) {
-        Element c = this.doc.createElement(child);
-        parent.appendChild(c);
-        return c;
-    }
-
-    public void addContent(Element e, String nodeName) {
-        e.appendChild(doc.createTextNode(nodeName));
-    }
-
+    /**
+     * give auto-increment to an element id
+     *
+     * @param e
+     */
     public void setIncId(Element e) {
         String c = this.root.getAttribute("counter");
         int x = Integer.parseInt(c) + 1;
@@ -107,12 +103,44 @@ public class XML {
         e.setAttribute("id", s);
     }
 
+    /**
+     * add child with no textnode to an element
+     *
+     * @param parent
+     * @param child
+     * @return
+     */
+    public Element addNewChild(Element parent, String child) {
+        Element c = this.doc.createElement(child);
+        parent.appendChild(c);
+        return c;
+    }
+
+    /**
+     * add textnode to an element
+     *
+     * @param e
+     * @param nodeName
+     */
+    public void addContent(Element e, String nodeName) {
+        e.appendChild(doc.createTextNode(nodeName));
+    }
+
+    /**
+     * combination of addNewChild() and addContent()
+     *
+     * @param parentNode
+     * @param elementName
+     * @param elementContent
+     */
     public void addElement(Element parentNode, String elementName, String elementContent) {
         Element e = this.doc.createElement(elementName);
         parentNode.appendChild(e);
         e.appendChild(doc.createTextNode(elementContent));
     }
-
+    /**
+     * write content to file
+     */
     public void writeContent() {
         try {
             //write the content into xml file
@@ -120,13 +148,15 @@ public class XML {
             //indent
             //t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
             t.setOutputProperty(OutputKeys.INDENT, "yes");
+            
             //transform doc into xml
             DOMSource source = new DOMSource(this.doc);
             StreamResult result = new StreamResult(this.file);
             t.transform(source, result);
+            
             // Output to console for testing
-            StreamResult consoleResult = new StreamResult(System.out);
-            t.transform(source, consoleResult);
+            //StreamResult consoleResult = new StreamResult(System.out);
+            //t.transform(source, consoleResult);
         } catch (Exception e) {
             e.printStackTrace();
         }

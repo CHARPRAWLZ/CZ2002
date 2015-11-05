ae904745d52cb18a0157b596ca503a4be124f500
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-05 21:00:26 +0800

    Showtime (in progress)

--- Showtime.java
+++ Showtime.java
@@ -1,5 +1,18 @@
 package CZ2002;
+import java.util.Date;
 
 public class Showtime {
     
+    private Cinema cinema;
+    private Movie movie;
+    private Date timing;
+    
+    public Showtime(Cinema cinema, Movie movie, Date timing){
+        
+    }
+    
+    /*public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println("Date: " + d.toString());
+    }*/
 }

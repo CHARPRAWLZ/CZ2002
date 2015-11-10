7fd22dacc06ad42a38956ee39864887559623bca
calvinlcz <calvinlee@Calvins-MacBook-Air.local>
2015-11-10 15:59:59 +0800

    Cineplex done (further testing with main is needed), Cinema is halfway completed, awaiting Layout class to be completed.

--- Cinema.java
+++ Cinema.java
@@ -1,23 +1,55 @@
 package CZ2002;
-
+/**
+ * Cinema class
+ * @author calvinlee
+ */
 public class Cinema {
 
-    private String cinemaCode;
+    private String name;
     //private Seat[] seat;
+    private String cinemaType;
     
-    public Cinema(String cinemaCode) {
-        this.cinemaCode = cinemaCode;
+    /**
+     * Constructor
+     * @param name 
+     */
+    public Cinema(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Returns the name of the Cinema
+     * @return 
+     */
+    public String getName() {
+        return this.name;
     }
 
+    /**
+     * Updates the name of the Cinema
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Returns the type of the Cinema
+     * @return 
+     */
+    public String getCinemaType() {
+        return this.cinemaType;
+    }
+    
+    /**
+     * Updates the type of the Cinema
+     * @param cinemaType 
+     */
+    public void setCinemaType(String cinemaType) {
+        this.cinemaType = cinemaType;
+    }
+    
     public void getLayout() {
         // enter code here
-    }
-
-    public String getName() {
-        return this.cinemaCode;
-    }
-
-    public void setName(String Name) {
-        this.cinemaCode = Name;
     }
 }

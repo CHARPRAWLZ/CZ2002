e0be6845b19502d2d7f0f2ad96c06ad8a454fe9d
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-11 00:45:50 +0800

    Hock's update

--- Payment.java
+++ Payment.java
@@ -10,18 +10,18 @@ package CZ2002;
  * @author calvinlee
  */
 public class Payment {
-    private String transactionID;
+    private String TID;
     
     public Payment(String cinemaName, String movieDate, String movieTime) {
-        
+        TID = cinemaName + generateDate(movieDate) + generateTime(movieTime);
     }
     
     private String generateDate(String date){
         char[] charArr = date.toCharArray();
         String str = "", str2 = "";
-        int i = 2;
+        int i = 3;
         for (char c : charArr){
-            if (c >= '0' || c <= '9'){
+            if (c >= '0' && c <= '9'){
                 str += String.valueOf(c);
             }
             else {
@@ -38,6 +38,42 @@ public class Payment {
     private String generateTime(String time){
         int size = time.length();
         char[] charArr = time.toCharArray();
-        int hours = Integer.valueOf(String.valueOf(charArr[0]));
+        String hours = "", mins = "", afternoon = "";
+        int i = 2;
+        for (char c : charArr) {
+            if (c < '0' || c > '9'){
+                i--;
+            }
+            else {
+                switch(i){
+                    case 2: hours += String.valueOf(c);
+                        break;
+                    case 1: mins += String.valueOf(c);
+                        break;
+                    case 0: afternoon += String.valueOf(c);
+                        i--;
+                        break;
+                }
+            }
+            System.out.println(hours + ", " + mins + ", " + afternoon);
+            if (i == -1)
+                break;
+        }
+        System.out.println(afternoon);
+        if (afternoon.equals("P")) {
+            int val = Integer.valueOf(hours);
+            val += 12;
+            hours = String.valueOf(val);
+        }
+        return hours + mins;
+    }
+    
+    public String getTID() {
+        return TID;
+    }
+    
+    public static void main(String[] args){
+        Payment pay = new Payment("CNPA", "11 Feb 2015, Fri", "01:35 PM");
+        System.out.println(pay.getTID());
     }
 }

28a968d23e787489f66c77dc75d650713cc62008
calvinlcz <calvinlee@Calvins-MacBook-Air.local>
2015-11-11 15:48:40 +0800

    TID format completed, along with javadoc for methods

--- Payment.java
+++ Payment.java
@@ -28,6 +28,7 @@ public class Payment {
      * @return formatted booked date of the movie for the TID
      */
     private String generateDate(String date){
+        /*
         char[] charArr = date.toCharArray();
         String str = "", str2 = "";
         int i = 3;
@@ -43,9 +44,63 @@ public class Payment {
                 str = "";
             }
         }
-        return str2;
+        return str2;*/
+        int index = 0;
+        String year, day, month;
+        for(int i = 0; i < date.length(); i++) {
+            char c = date.charAt(i);
+            if(c == ',')
+                index = i;
+        }
+        String str = date.substring(0, index);
+        String[] arr = str.split(" ");
+        day = arr[0];
+        month = arr[1];
+        year = arr[2];
+        if(month.equalsIgnoreCase("Jan")) {
+            month = "01";
+        }
+        else if(month.equalsIgnoreCase("Feb")) {
+            month = "02";
+        }
+        else if(month.equalsIgnoreCase("Mar")) {
+            month = "03";
+        }
+        else if(month.equalsIgnoreCase("Apr")) {
+            month = "04";
+        }
+        else if(month.equalsIgnoreCase("May")) {
+            month = "05";
+        }
+        else if(month.equalsIgnoreCase("Jun")) {
+            month = "06";
+        }
+        else if(month.equalsIgnoreCase("Jul")) {
+            month = "07";
+        }
+        else if(month.equalsIgnoreCase("Aug")) {
+            month = "08";
+        }
+        else if(month.equalsIgnoreCase("Sep")) {
+            month = "09";
+        }
+        else if(month.equalsIgnoreCase("Oct")) {
+            month = "10";
+        }
+        else if(month.equalsIgnoreCase("Nov")) {
+            month = "11";
+        }
+        else if(month.equalsIgnoreCase("Dec")) {
+            month = "12";
+        }
+        return year + month + day;
     }
     
+    /**
+     * Returns a String that consist of time data to generate the TID
+     * @param time booked time of the movie for the TID
+     * @return formatted booked time of the movie for the TID
+     */
     private String generateTime(String time){
         //int size = time.length();
         char[] charArr = time.toCharArray();
@@ -66,7 +121,7 @@ public class Payment {
                         break;
                 }
             }
-            System.out.println(hours + ", " + mins + ", " + afternoon);
+            //System.out.println(hours + ", " + mins + ", " + afternoon);
             if (i == -1)
                 break;
         }
@@ -79,12 +134,16 @@ public class Payment {
         return hours + mins;
     }
     
+    /**
+     * Returns the TID
+     * @return TID
+     */
     public String getTID() {
         return TID;
     }
-    
+    /*
     public static void main(String[] args){
         Payment pay = new Payment("CNPA", "11 Feb 2015, Fri", "01:35 PM");
         System.out.println(pay.getTID());
-    }
+    }*/
 }

7a14e73ceda751dbb55cd38800c338f5194f89d9
zacharias60 <zacharias60@gmail.com>
2015-11-11 16:07:21 +0800

    employee and user change
    user now has username and password, but have different methods from
    employee. user can rate, review and book movies, while employee has
    additional methods.

--- Employee.java
+++ Employee.java
@@ -6,48 +6,20 @@ author: zach
 */
 import java.util.Scanner;
 public class Employee extends User{
-    //variables
-    private String userName, password;
     //constructor for employee
     /**
     * Constructs an instance of class Employee.
     * @param user
-    * @param  pw
+    * @param pw
+    * @param id
+    * @param name
+    * @param email
+    * @param number
+    * @param age
     */
-    public Employee(String user, String pw, String id, String name, String email,String number){
-        super(id,name,email,number);
-        this.userName = user;
-        this.password = pw;
-    }
-    //accessors
-    /**
-     * Returns the username of the employee account
-     * @return userName
-     */
-    public String getUserName(){
-        return userName;
-    }
-    /**
-     * Returns the password of the employee account
-     * @return password
-     */
-    public String getPassword(){
-        return password;
-    }
-    //mutators
-    /**
-     * sets the username of the employee account
-     * @param user 
-     */
-    public void setUserName(String user){
-        userName = user;
-    }
-    /**
-     * sets the password of the employee account
-     * @param pw 
-     */
-    public void setPassword(String pw){
-        password = pw;
+    public Employee(String user, String pw, String id, String name, String email,String number,int age){
+        super(user,pw,id,name,email,number,age);
+        
     }
     
     //methods for employee
--- User.java
+++ User.java
@@ -4,22 +4,46 @@ author: zach
 */
 public class User {
     //variables
-   private String userId, name, emailAddress, mobileNo;
+   private String userId, name, emailAddress, mobileNo, userName, password;
+   private int age;
    //constructors
    /**
     * Constructs an instance of class User.
+    * @param user
+    * @param pw
     * @param id
     * @param name
     * @param email
     * @param number 
+    * @param age
     */
-   public User(String id,String name,String email,String number){
+   public User(String user, String pw, String id, String name, String email,String number,int age){
+        this.userName = user;
+       this.password = pw;
        this.userId = id;
        this.name = name;
        this.emailAddress = email;
        this.mobileNo = number;
+       this.age = age;
+      
    }
    //accessors
+   /**
+     * Returns the username of the user account
+     * @return userName
+     */
+    public String getUserName(){
+        return userName;
+    }
+    /**
+     * Returns the password of the user account
+     * @return password
+     */
+    public String getPassword(){
+        return password;
+    }
+    //mutators
+    
    /**
     * returns the UserID of the User instance.
     * @return userId
@@ -48,13 +72,34 @@ public class User {
    public String getMobileNumber(){
         return mobileNo;
    }
+   /**
+    * returns the age of the user
+    * @return age
+    */
+   public int getAge(){
+       return age;
+   }
    //mutators
+  /**
+   * sets the username of the user account
+   * @param user 
+   */
+    public void setUserName(String user){
+        userName = user;
+    }
+   /**
+    * sets the password of the user account
+    * @param pw 
+    */
+    public void setPassword(String pw){
+        password = pw;
+    }
    /**
     * set the user's name
     * @param uname 
     */
-   public void setName(String uname){
-        name = name;
+   public void setName(String myName){
+        name = myName;
    }
    /**
     * set the user's email
@@ -70,4 +115,28 @@ public class User {
    public void setMobileNumber(String number){
         mobileNo = number;
    }
+   /**
+    * sets the user's age in terms of years
+    * @param years 
+    */
+   public void setAge(int years){
+       age = years;
+   }
+   //methods for user
+   public Movie movie;
+   /**
+    * user rates a certain movie
+    * @param rating
+    */
+   public void rateMovie(double rating){
+       movie.setOverallRating(rating);
+   }
+   /**
+    * user reviews a movie
+    */
+   public void reviewMovie(){}
+   /**
+    * user books a movie
+    */
+   public void bookMovie(){}
 }

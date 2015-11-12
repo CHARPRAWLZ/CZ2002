ef4bf9d67650256410c2cb1eca6502e42b56965c
agunimon001 <C140149@e.ntu.edu.sg>
2015-11-13 03:48:25 +0800

    New Control Class

---
+++ CinemaMgr.java
@@ -0,0 +1,34 @@
+package CZ2002;
+
+public class CinemaMgr {
+
+    Cineplex[] cineplex = new Cineplex[3];
+    
+    public CinemaMgr(){
+        cineplex[0] = new Cineplex("GV", "Golden Village", "Vivo City", 6);
+        cineplex[1] = new Cineplex("CTH", "Cathay", "Orchard", 7);
+        cineplex[2] = new Cineplex("SHW", "SHAW", "Dhoby Ghaut", 6);
+    }
+    
+    public String[] getCineplexList() {
+        String[] cineplexName = new String[3];
+        for (int i = 0; i < 3; i++){
+            cineplexName[i] = cineplex[i].getName();
+        }
+        return cineplexName;
+    }
+    
+    public Cinema[] getCinemaList(int choice){
+        int size = cineplex[choice].getNumOfCinema();
+        Cinema[] cinema = new Cinema[3];
+        for (int i = 0; i < size; i++){
+            cinema[i] = cineplex[choice].getCinemaList().get(i);
+        }
+        return cinema;
+    }
+    
+    public Cinema getCinema(int cineplex, int cinemaChoice){
+        return getCinemaList(cineplex)[cinemaChoice];
+    }
+    
+}
